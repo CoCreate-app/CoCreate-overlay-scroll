4f06622a4129a576afd515f42d7c318403539872
Frank <frank@cocreate.app>
2022-02-24 19:10:28 +0000

    fix: CoCreate.config replace CoCreate.app with *

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -14,7 +14,7 @@ module.exports = {
                 "name": "index.html",
                 "path": "/docs/overlay-scroll/index.html",
                 "domains": [
-                    "cocreate.app",
+                    "*",
                     "general.cocreate.app"
                 ],
                 "directory": "/docs/overlay-scroll",
