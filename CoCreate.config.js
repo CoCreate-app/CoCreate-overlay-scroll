d27bd0ff55ec2975f0da4a6a9fc277bff936d9c6
frankpagan <frank@cocreate.app>
2023-09-17 19:11:35 -0600

    fix:  Add path and pathname

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -8,17 +8,16 @@ module.exports = {
             "object": {
                 "_id": "6020801d3c260207bf49caba",
                 "name": "index.html",
-                "path": "/docs/overlay-scroll/index.html",
+                "path": "/docs/overlay-scroll",
+                "pathname": "/docs/overlay-scroll/index.html",
                 "src": "{{./docs/index.html}}",
                 "host": [
                     "*",
                     "general.cocreate.app"
                 ],
-                "directory": "/docs/overlay-scroll",
-                "parentDirectory": "{{parentDirectory}}",
+                "directory": "overlay-scroll",
                 "content-type": "{{content-type}}",
-                "public": "true",
-                "website_id": "644d4bff8036fb9d1d1fd69c"
+                "public": "true"
             }
         }
     ]
